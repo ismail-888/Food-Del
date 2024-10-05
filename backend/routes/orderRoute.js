0bd07b1c5939512464dc9aa1edca0a328c65f8dd
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-10-05 14:59:07 +0300

    add my orderpages and connect with server

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { placeOrder, verifyOrder } from '../controllers/orderController.js';
+import { placeOrder, userOrders, verifyOrder } from '../controllers/orderController.js';
 import authMiddleware from '../middleware/auth.js'
 
 const orderRoute=express.Router();
 
 orderRoute.post("/place",authMiddleware,placeOrder)
 orderRoute.post("/verify",verifyOrder)
+orderRoute.post("/userorders",authMiddleware,userOrders)
 
 export default orderRoute
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,11 +1,14 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./PlaceOrder.css";
 import { StoreContext } from "../../context/StoreContext";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const PlaceOrder = () => {
   const { getTotalCartAmount, token, food_list, cartItems, url } =
     useContext(StoreContext);
+
+  const navigate = useNavigate();
 
   const [data, setData] = useState({
     firstName: "",
@@ -50,6 +53,14 @@ const PlaceOrder = () => {
       alert("Error");
     }
   };
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/cart");
+    } else if (getTotalCartAmount() === 0) {
+      navigate("/cart");
+    }
+  }, [token]);
 
   return (
     <form onSubmit={placeOrder} className="place-order">
