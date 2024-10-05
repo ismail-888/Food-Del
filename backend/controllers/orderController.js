139c73e045ab89d847481303152bbb992b265054
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-10-05 15:59:13 +0300

    finish track order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,10 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placiing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+
+  // todo lezm l frontend_url ykoun nafess l url tab3 l frontend ma l admin
+  const frontend_url = "http://localhost:5174";
+
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
@@ -80,4 +83,28 @@ const userOrders = async (req, res) => {
   }
 };
 
-export { placeOrder, verifyOrder, userOrders };
+// Listing orders for admin panel
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+// api for updating order status
+const updateStatus = async (req, res) => {
+  try {
+    await orderModel.findByIdAndUpdate(req.body.orderId, {
+      status: req.body.status,
+    });
+    res.json({ success: true, message: "Status Updated" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { placeOrder, verifyOrder, userOrders, listOrders, updateStatus };
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { placeOrder, userOrders, verifyOrder } from '../controllers/orderController.js';
+import { listOrders, placeOrder, updateStatus, userOrders, verifyOrder } from '../controllers/orderController.js';
 import authMiddleware from '../middleware/auth.js'
 
 const orderRoute=express.Router();
@@ -7,5 +7,7 @@ const orderRoute=express.Router();
 orderRoute.post("/place",authMiddleware,placeOrder)
 orderRoute.post("/verify",verifyOrder)
 orderRoute.post("/userorders",authMiddleware,userOrders)
+orderRoute.get("/list",listOrders)
+orderRoute.post("/status",updateStatus)
 
 export default orderRoute
