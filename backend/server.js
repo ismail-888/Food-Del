a59b87d4d5f38821df311d7b028ff53646b3745e
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-10-06 07:45:50 +0300

    l

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,35 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRoute from "./routes/orderRoute.js";
 
+//app config
 const app = express();
 const port = 4000;
 
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// db connection
+connectDB();
+
+// api endpoints
+// app.use("/api/food", foodRouter);
+// app.use("/images", express.static("uploads"));
+// app.use("/api/user", userRouter);
+// app.use("/api/cart", cartRouter);
+// app.use("/api/order", orderRoute);
+
 app.get("/", (req, res) => {
-  res.send("Hello from Express!");
+  res.send("API Working");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server Started on http://localhost:${port}`);
 });
+// wVszwJiN3K1d5LcG
