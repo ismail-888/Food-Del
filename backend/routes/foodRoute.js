3cc81fe9076a00b9d698ff67cce9ed9cce5e7a24
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-10-06 08:30:21 +0300

    1

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,17 +1,17 @@
 import express from "express";
 const foodRouter = express.Router();
 import { addFood, listFood, removeFood } from "../controllers/foodController.js";
-import multer from "multer";
+// import multer from "multer";
 
 
 // Image Storage Engine
 
-const storage = multer.diskStorage({
-  destination: "uploads",
-  filename: (req, file, cb) => {
-    return cb(null, `${Date.now()}${file.originalname}`);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: "uploads",
+//   filename: (req, file, cb) => {
+//     return cb(null, `${Date.now()}${file.originalname}`);
+//   },
+// });
 
 // const upload = multer({ storage: storage });
 
