1c1ba5a153195a874ab7cd23597017ae3811c3b1
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-10-06 08:20:15 +0300

    2

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,8 @@ import cors from "cors";
 import { connectDB } from "./config/db.js";
 // import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
-// import cartRouter from "./routes/cartRoute.js";
-// import orderRoute from "./routes/orderRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRoute from "./routes/orderRoute.js";
 
 //app config
 const app = express();
@@ -22,8 +22,8 @@ connectDB();
 // app.use("/api/food", foodRouter);
 // app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
-// app.use("/api/cart", cartRouter);
-// app.use("/api/order", orderRoute);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRoute);
 
 app.get("/", (req, res) => {
   res.send("API Working");
